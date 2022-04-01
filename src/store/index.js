fcef199fafda5616dba5d18fee4e2ca9d8cf47dc
brayanAtalaya <developeratalaya3@outlook.com>
2022-04-01 13:41:12 -0500

    add nodeEnv utils

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,10 @@
 import { createStore, applyMiddleware, compose } from 'redux'
 import rootReducer from './reducer'
 import thunk from 'redux-thunk'
+import { nodeEnv } from 'utils/constant'
 // ===========================|| REDUX - MAIN STORE ||=========================== //
 const composeEnhancers =
-	(process.env.NODE_ENV !== 'production' &&
+	(nodeEnv !== 'production' &&
 		typeof window !== 'undefined' &&
 		window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
 	compose
--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,1 +1,2 @@
 export const baseURL = process.env.REACT_APP_BASE_URL
+export const nodeEnv = process.env.REACT_APP_NODE_ENV
