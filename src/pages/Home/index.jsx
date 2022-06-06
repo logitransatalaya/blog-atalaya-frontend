ea6c15e15e62e97a461cd0c09688c28e235c3be4
brayanAtalaya <developeratalaya3@outlook.com>
2022-06-06 14:13:20 -0500

    fix build

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -5,32 +5,15 @@ import { MODAL_OPEN } from 'store/actions'
 import { useDispatch } from 'react-redux'
 import Carousel from 'components/Carousel/'
 import SocialMedia from 'components/SocialMedia'
-import axios from 'axios'
 
 const Home = () => {
 	const dispatch = useDispatch()
-	const [datas, setDatas] = useState([])
 	const handleModal = (val) => {
 		dispatch({ type: MODAL_OPEN, modalOpen: val })
 	}
-	const getData = async () => {
-		const { data } = await axios.get(
-			`https://blog-atalaya-back.herokuapp.com/api/v1/allies`
-		)
-		setDatas(data)
-	}
-	useEffect(() => {
-		getData()
-		//setData(data)
-	}, [])
 
 	return (
 		<HomeStyles>
-			<div>
-				{datas.map((item) => (
-					<h1 key={`ls${item.id}`}>{item.name}</h1>
-				))}
-			</div>
 			<div className='searchbox'>
 				<Search handleSearch={() => handleModal(true)} />
 			</div>
--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,4 +1,4 @@
 /* create file .env.production */
-export const baseURL = process.env.REACT_APP_BASE_URL
-export const nodeEnv = process.env.REACT_APP_NODE_ENV
+export const baseURL = 'https://blog-atalaya-back.herokuapp.com'
+export const nodeEnv = process.env.REACT_APP_NODE_ENV || 'production'
 export const limit = 10
